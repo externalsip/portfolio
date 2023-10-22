b92410e3d51f9117eeaf2084094899199f123f1b
externalsip <93773873+externalsip@users.noreply.github.com>
2023-10-21 20:50:53 -0400

    Big commit
    -Added 3 projects
    -Made page responsive in landscape
    -added a main page with info such as socials and email.
    -Fixed swiper
    -Added the info modal under project for important info.
    -Added a small visual witness to announce that the text is done displaying if the current page isnt the last one.
    Added dialog for every projects.

--- script/script.js
+++ script/script.js
@@ -1,15 +1,13 @@
-let textboxText = document.querySelector(".textboxText");
+let textboxText = document.querySelector(".textbox__Text");
+let textboxNext = document.querySelector(".textbox__Next");
 let textbox = document.querySelector(".textbox");
-let pageNum = 0;
-let projectIndex = 0;
+let pageNum;
+let projectIndex;
 let projectSwap;
 let currentPage;
 let currentSlide;
 let currentProject = document.querySelector(".swiperPortfolio");
-let clickArea = document.querySelectorAll(".clickArea");
-let buttonLast = document.querySelector(".buttonLast");
-let buttonNext = document.querySelector(".buttonNext");
-let swipercontainer = document.getElementById("swiper-container");
+
 let json, to;
 const dialogData = './json/dialog.json';
 
@@ -19,9 +17,21 @@ const dialogData = './json/dialog.json';
 // dialog is the table in which the text is held
 // currentPage is the page that is currently on screen 
 
+const openBtn = document.getElementById("openPortfolio");
+let mainScreen = document.querySelector(".titleScreen");
+let vn = document.querySelector(".wrapper");
+
+openBtn.addEventListener("click", () => {
+	mainScreen.style.display = "none";
+	vn.style.display = "block";
+	initialize(json);
+});
+
 async function grabData() {
 	const resp = await fetch(dialogData);
 	json = await resp.json();
+	pageNum = 0;
+	projectIndex = 0;
 	projectSwap = Object.keys(json.projects)[projectIndex];
 	currentPage = Object.keys(json.projects[projectSwap].dialog)[pageNum];
     initialize(json);
@@ -69,6 +79,7 @@ function typeWriter(txt, i, wordsArr, currentWord) {
   
 	if (stopText === true) {
 	  i = wordsArr.length;
+	  checkNext();
 	  isTextDisplaying = false;
 	  handleAnimation();
 	}
@@ -76,7 +87,10 @@ function typeWriter(txt, i, wordsArr, currentWord) {
 		isTextDisplaying = true;
 		handleAnimation();
 		if (currentWord < wordsArr.length) {
-
+			//Reset state of fade animation
+			fade.pause();			
+			gsap.set(textboxNext, {opacity: 0});
+			//Finds current word, how many characters said word has, then sets that value as "i" to then look at what character it currently has to write by looking at what is already there, then writes the character.
 			var word = wordsArr[currentWord];
 	  		var charsToShow = i > word.length ? word.length : i;
 	  		var visibleWord = word.slice(0, charsToShow);
@@ -103,6 +117,7 @@ function typeWriter(txt, i, wordsArr, currentWord) {
 	  }, speed);
 	} 
 	else {
+		checkNext();
 		isTextDisplaying = false;
 		handleAnimation();
 	}
@@ -116,16 +131,31 @@ function typeWriter(txt, i, wordsArr, currentWord) {
 	if(data.projects[projectSwap].dialog[currentPage].hasOwnProperty('NextPage')) {
 		if(data.projects[projectSwap].dialog[currentPage].NextPage == "End") return false;
 	}
-	
 	return true;
 }
 
-//Function manages clicks on the screen
+//If the text stops displaying and the page is not the last in the chapter, the fade animation plays.
+function checkNext(){
+	if(json.projects[projectSwap].dialog[currentPage].hasOwnProperty('NextPage')) {
+		if(json.projects[projectSwap].dialog[currentPage].NextPage == "End"){};
+	}
+	else{
+		fade.play(0);
+	}
+}
 
-  clickArea.forEach((element) => {
+let fade = gsap.to(textboxNext, {opacity: 1, duration: 1, repeat: -1, yoyo: true, ease: "sine.inOut" });
+fade.pause();
+
+//Function manages clicks on the screen
+let clickArea = document.querySelectorAll(".clickArea");
+let buttonLast = document.querySelector(".buttonLast");
+let buttonNext = document.querySelector(".buttonNext");
+
+clickArea.forEach((element) => {
 	element.addEventListener("click", () => {
 		if(isTextDisplaying == true){
-			initialize(json);
+				initialize(json);
 		}
 		else{
 			if(!json) return;
@@ -136,6 +166,10 @@ function typeWriter(txt, i, wordsArr, currentWord) {
 					projectSwap = Object.keys(json.projects)[projectIndex];
 					currentPage = Object.keys(json.projects[projectSwap].dialog)[pageNum];
 					manageSlides(json);
+					manageModal(json);
+					buttonLast.classList.remove("disabledBtn");
+					buttonNext.classList.remove("disabledBtn");
+					textbox.style.width = "70%";
 					buttonLast.removeAttribute("disabled");
 					buttonNext.removeAttribute("disabled");
 				}
@@ -144,15 +178,18 @@ function typeWriter(txt, i, wordsArr, currentWord) {
 					pageNum++;
 					currentPage = Object.keys(json.projects[projectSwap].dialog)[pageNum];
 				}
-					initialize(json);
+				initialize(json);
 			}
 		}
 	})
-  });
+});
 
   //2 following event listeners manage what happens when you click on the button for the previous or next project in the list.
 
+
+//Previous project
 buttonLast.addEventListener("click", () => {
+	isTextDisplaying = false;
 	if(projectIndex == 1){
 		pageNum = 0;
 		projectIndex = 4;
@@ -166,10 +203,13 @@ buttonLast.addEventListener("click", () => {
 		currentPage = Object.keys(json.projects[projectSwap].dialog)[pageNum];
 	}		
 	manageSlides(json);
+	manageModal(json);
 	initialize(json);
 });
 
+//Next project
 buttonNext.addEventListener("click", () => {
+	isTextDisplaying = false;
 	if(projectIndex == 4){
 		pageNum = 0;
 		projectIndex = 1;
@@ -183,16 +223,17 @@ buttonNext.addEventListener("click", () => {
 		currentPage = Object.keys(json.projects[projectSwap].dialog)[pageNum];
 	}
 	manageSlides(json);
+	manageModal(json);
 	initialize(json);
 });
 
-// GSAP animations on spritesheets.
+// GSAP animations on spritesheets and the icon that fades in and out when there is a next slide.
 
-let talk = gsap.to(".sprite__mouth", {backgroundPositionX: "-45vw", ease: SteppedEase.config(3), duration: 0.5, repeat: -1});
-let blink = gsap.to(".sprite__eyes", {backgroundPositionX: "-60vw", ease: SteppedEase.config(4), duration: 0.2, repeat: -1, repeatDelay: 5});
+let talk = gsap.to(".sprite__mouth", {backgroundPositionX: "-300%", ease: SteppedEase.config(3), duration: 0.5, repeat: -1});
+let blink = gsap.to(".sprite__eyes", {backgroundPositionX: "-400%", ease: SteppedEase.config(4), duration: 0.2, repeat: -1, repeatDelay: 5});
+
 
 talk.pause();
-
 //Function handles the talking animation, if text is currently being displayed, the animation will loop, it resets when the text stops displaying.
 
 function handleAnimation(){
@@ -211,17 +252,20 @@ var swiperPortfolio = new Swiper('.swiperPortfolio', {
 	// Optional parameters
 	direction: 'horizontal',
 	loop: true,
-	autoplay: {
-		delay:2500,
-		disableOnInteraction: false,
-		pauseOnMouseEnter: true,
-	},
 	slidesPerView: 1,
+	navigation: {
+        nextEl: ".swiperPortfolio__nextSlide",
+        prevEl: ".swiperPortfolio__lastSlide",
+      },
 });
 	
 //Function manages the slides currently present in the swiper, everytime it is called, it starts by removing the slides currently in the slider.
 
 //Content is the object in which the slides are contained inside of the json
+
+//At first tried to create the slides manually, kinda worked but they couldnt figure out what order they were supposed to have, then learned there was a line built-in swiper.js to add slides, so I just used that instead.
+let swipercontainer = document.getElementById("swiper-container");
+let swipeBtn = document.querySelectorAll(".swiperPortfolio__button");
 
 function manageSlides(data){
 	swipercontainer.innerHTML = "";
@@ -230,7 +274,7 @@ function manageSlides(data){
 		//const swipePage = document.createElement("div");
 		//swipePage.classList.add("swiper-slide");
 		if(data.projects[projectSwap].content[currentSlide].type == "video"){			
-			swiperPortfolio.appendSlide(('<div class="swiper-slide")><iframe loading="lazy" src= '+ data.projects[projectSwap].content[currentSlide].source +'</div>'));
+			swiperPortfolio.appendSlide(('<div class="swiper-slide")><iframe src= '+ data.projects[projectSwap].content[currentSlide].source +'</div>'));
 			//swipePage.style.backgroundImage = "url('" + data.projects[projectSwap].content[currentSlide].source + "')";
 			//swipercontainer.appendChild(swipePage);
 		}
@@ -242,7 +286,106 @@ function manageSlides(data){
 			//swipercontainer.appendChild(swipePage);
 		}
 	}
+	if(data.projects[projectSwap].length == 1){
+		swipeBtn.forEach(element => element.style.display = "none");
+	}
+	else{
+		swipeBtn.forEach(element => element.style.display = "block");
+	}
 }
+
+//Function manages what is contained inside the modal, info changes depending on the project
+
+
+function manageModal(data){
+	let modalTitle = document.querySelector(".modal__title");
+	let modalRole = document.querySelector(".modal__role");
+	let modalDesc = document.querySelector(".modal__description");
+	let softwareList = document.querySelector(".softwareList");
+	let mediaList = document.querySelector(".mediaList");
+	modalTitle.innerHTML = data.projects[projectSwap].modalContent.name;
+	modalRole.innerHTML = data.projects[projectSwap].modalContent.role;
+	modalDesc.innerText = data.projects[projectSwap].modalContent.desc;
+	softwareList.innerHTML = "";
+	mediaList.innerHTML = "";
+	//loops on the elements present in the array software in the JSON
+	for(let i = 0; i <= data.projects[projectSwap].modalContent.software.length - 1; i++){
+		const li = document.createElement("li");
+		li.classList.add("modal__list__element");
+		const node = document.createTextNode(data.projects[projectSwap].modalContent.software[i]);
+		li.appendChild(node);
+		softwareList.appendChild(li);
+	}
+	//loops in steps of 2 on the elements present in the media array in JSON
+	//This has to be the goofiest loop I've ever written, let me cook.
+	for(let i = 0; i + 1 <= data.projects[projectSwap].modalContent.media.length; i = i + 2){
+		const li = document.createElement("li");
+		li.classList.add("modal__list__element");
+		li.innerHTML = "<a href= '" + data.projects[projectSwap].modalContent.media[i + 1] + "'> "+ data.projects[projectSwap].modalContent.media[i] +" </a>";
+		mediaList.appendChild(li);
+	}
+}
+
+//Functions display and hide modal in main menu
+
+let modalMainArea = document.querySelector(".modalMain__backdrop");
+let infoMainBtn = document.querySelector("#openInfo");
+let modalMainBtn = document.querySelector(".modalMain__btn");
+let modalMain = document.querySelector(".modalMain");
+
+modalMainArea.addEventListener("click", closeMainModal);
+modalMainBtn.addEventListener("click", closeMainModal);
+
+function closeMainModal(){
+	gsap.timeline()
+	.to(modalMain, {opacity: 0, duration: 0.5})
+	.to(modalMainArea, {opacity: 0, duration: 0.5}, "<")
+	.set(modalMain, {display: "none"})
+	.set(modalMainArea, {display: "none"}, ">");
+};
+
+infoMainBtn.addEventListener("click", openMainModal);
+
+function openMainModal(){
+	gsap.timeline()
+	.set(modalMain, {display: "flex"})
+	.set(modalMainArea, {display: "block"}, "<")
+	.to(modalMain, {opacity: 1, duration: 0.5})
+	.to(modalMainArea, {opacity: 1, duration: 0.5}, "<");
+}
+
+
+
+//Functions display and hide modal in vn
+
+let modalArea = document.querySelector(".modal__backdrop");
+let infoBtn = document.querySelector(".swiperPortfolio__info");
+let modalBtn = document.querySelector(".modal__btn");
+let modal = document.querySelector(".modal");
+
+
+modalArea.addEventListener("click", closeModal);
+modalBtn.addEventListener("click", closeModal);
+
+function closeModal(){
+	gsap.timeline()
+	.to(modal, {opacity: 0, duration: 0.5})
+	.to(modalArea, {opacity: 0, duration: 0.5}, "<")
+	.set(modal, {display: "none"})
+	.set(modalArea, {display: "none"}, ">");
+};
+
+infoBtn.addEventListener("click", openModal);
+
+function openModal(){
+	gsap.timeline()
+	.set(modal, {display: "flex"})
+	.set(modalArea, {display: "block"}, "<")
+	.to(modal, {opacity: 1, duration: 0.5})
+	.to(modalArea, {opacity: 1, duration: 0.5}, "<");
+}
+
+
 
 //calls grabdata on execution of script, will most likely be modified later if I decide to make a title screen (would most likely be the best way to add the required info without it feeling forced)
 
